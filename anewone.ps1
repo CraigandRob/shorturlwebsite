5053864f12b6e492bac36c3f7e24fb6c570744dd
Rob Sewell <mrrobsewell@outlook.com>
2025-02-28 18:34:51 +0000

    Update Redirect URL and Template Replacement
    Correct the URL in the redirect configuration and improve the template replacement logic in the script to ensure proper URL formatting.

--- anewone.ps1
+++ anewone.ps1
@@ -4,5 +4,5 @@ $slashtag = 'rob'
 $template = Get-Content -Path template.md
 $filepath = "{0}.md" -f $slashtag -replace '/',''
 
-$newTemplate = $template -replace '__URL__',$url.'Destination URL'  -replace '__LINKWITHSLASH__',$slashtag
+$newTemplate = $template -replace '__URL__',$url  -replace '__LINKWITHSLASH__',$slashtag
 $newTemplate | Set-Content -Path  $filepath
